0569a1db5c507553c71cde0481ee79830d7d33b2
Sergey <kapcov@yandex.ru>
2018-04-28 10:49:25 +0300

    initial

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input() {
+    return (
+        <div className="active">
+            <input type="checkbox" name="checkTask"/>
+            <input type="text"/>
+            <button>edit</button>
+            <button>delete</button>
+        </div>
+    )
+}
